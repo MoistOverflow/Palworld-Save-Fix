7a996fe5b862d9e96b051ea0413b167039e24d59
moist <admin@dfessler.com>
2024-02-01 01:34:42 -0500

    init

---
+++ README.md
@@ -0,0 +1,42 @@
+# Palworld Host Save Fix No uesave
+
+> ### Be careful of data loss and *always* make a backup.
+
+Dependencies:
+- Python 3
+
+Command:    
+`python fix-savs.py <old_guid> <new_guid>`
+
+`<new_guid>` - GUID of the player on the new server    
+`<old_guid>` - GUID of the player from the old server
+
+Example:    
+`python fix-host-save.py 00000000000000000000000000000001 6A80B1A6000000000000000000000000`
+
+## migrate a co-op save to a Linux dedicated server
+
+Prerequisites:
+- Install the dependencies [above].
+- The dedicated server is installed, running, and you're able to join it.
+- No viewing cage support.
+
+Steps:
+1. Copy your desired save's folder from `C:\Users\<username>\AppData\Local\Pal\Saved\SaveGames\<random_numbers>` to your dedicated server.
+2. In the `PalServer\Pal\Saved\Config\LinuxServer\GameUserSettings.ini` file, change the `DedicatedServerName` to match your save's folder name. For example, if your save's folder name is `2E85FD38BAA792EB1D4C09386F3A3CDA`, the `DedicatedServerName` changes to `DedicatedServerName=2E85FD38BAA792EB1D4C09386F3A3CDA`.
+3. Delete `PalServer\Pal\Saved\SaveGames\0\<your_save_here>\WorldOption.sav` to allow modification of `PalWorldSettings.ini`. Players will have to choose their respawn point again, but nothing else is affected as far as I can tell.
+4. Confirm you can connect to your save on the dedicated server and that the world is the one in the save. You can check the world with a character that belongs to a regular player from the co-op.
+5. Afterwards, the co-op host must create a new character on the dedicated server. A new `.sav` file should appear in `PalServer\Pal\Saved\SaveGames\0\<your_save_here>\Players`.
+6. The name of that new `.sav` file is the co-op host's new GUID. We will need the co-op host's new GUID for the script to work.
+7. Shut the server down and then copy `PalServer\Pal\Saved\SaveGames\0\<your_save_here>/Level.sav` and `PalServer\Pal\Saved\SaveGames\0\<your_save_here>/Players/<co-op_sav_file>` to the `sav/` folder in the tool
+8. **Make a backup of your save!** This is an experimental script and has known bugs so always keep a backup copy of your save.
+9. Run the script using the command in the [Usage section](#usage) with the information you've gathered and using `00000000000000000000000000000001` as the co-op host's old GUID.
+10. Copy the save from the temporary folder back to the dedicated server. Move the save you had in the dedicated server somewhere else or rename it to something different.
+11. Start the server back up and have the co-op host join the server with their fixed character.
+
+
+
+
+This uses cheahjs https://github.com/cheahjs/palworld-save-tools for converting sav to json and back.
+
+Steps from xNul https://github.com/xNul/palworld-host-save-fix
---
+++ fix-savs.py
@@ -0,0 +1,122 @@
+import sys
+import json
+import os
+import re
+
+from lib.gvas import GvasFile
+from lib.noindent import CustomEncoder
+from lib.palsav import compress_gvas_to_sav, decompress_sav_to_gvas
+from lib.paltypes import PALWORLD_CUSTOM_PROPERTIES, PALWORLD_TYPE_HINTS
+
+
+def main(sav_file: str, guid: str):
+    if sav_file[-4:] == ".sav":
+        sav_file = sav_file[:-4]
+    if guid[-4:] == ".sav":
+        guid = guid[:-4]
+
+    convert_sav_to_json(filename=f"savs/{sav_file}.sav", output_path=f"./savs/{sav_file}.json")
+    # do work on user.sav
+    edit_user_json(sav_file, guid)
+
+    convert_json_to_sav(filename=f"savs/{sav_file}.json", output_path=f"./savs/{guid}.sav")
+    os.remove(f"savs/{sav_file}.json")
+
+    convert_sav_to_json(filename="savs/Level.sav", output_path="./savs/Level.json")
+    os.remove("savs/Level.sav")
+    # do work on level.sav
+    edit_level_json(sav_file, guid)
+
+    convert_json_to_sav(filename="savs/Level.json", output_path="./savs/Level.sav")
+    os.remove("savs/Level.json")
+
+
+def format_id_string(guid: str):
+    return f"{guid[0:8]}-{guid[8:12]}-{guid[12:16]}-{guid[16:20]}-{guid[20:]}"
+
+
+def edit_user_json(old_id: str, new_id: str):
+    filename = f"savs/{old_id}.json"
+    old_id = format_id_string(old_id)
+    new_id = format_id_string(new_id)
+    with open(filename, "r+") as old_file:
+        data = str(json.load(old_file))
+    new_data = eval(re.sub(old_id, new_id, data, flags=re.I))  # eval(data.replace(old_id, new_id))
+    os.remove(filename)
+    with open(filename, 'w') as new_file:
+        indent = "\t"
+        json.dump(new_data, new_file, indent=indent)
+
+
+def edit_level_json(old_id: str, new_id: str):
+    filler_id = "00000000-0000-0000-0000-000000000006"
+    filename = "savs/Level.json"
+    old_id = format_id_string(old_id)
+    new_id = format_id_string(new_id)
+    with open(filename, "r+") as old_file:
+        data = str(json.load(old_file))
+    temp_data = re.sub(new_id, filler_id, data, flags=re.I)  # data.replace(new_id, filler_id)
+    new_data = eval(re.sub(old_id, new_id, temp_data, flags=re.I))  # eval(temp_data.replace(old_id, new_id))
+    os.remove(filename)
+    with open(filename, 'w') as new_file:
+        indent = "\t"
+        json.dump(new_data, new_file, indent=indent)
+
+
+def convert_sav_to_json(filename: str, output_path: str):
+    minify = False
+    print(f"Converting {filename} to JSON, saving to {output_path}")
+    if os.path.exists(output_path):
+        print(f"{output_path} already exists, this will overwrite the file")
+        if not confirm_prompt("Are you sure you want to continue?"):
+            exit(1)
+    print(f"Decompressing sav file")
+    with open(filename, "rb") as f:
+        data = f.read()
+        raw_gvas, _ = decompress_sav_to_gvas(data)
+    print(f"Loading GVAS file")
+    gvas_file = GvasFile.read(raw_gvas, PALWORLD_TYPE_HINTS, PALWORLD_CUSTOM_PROPERTIES)
+    print(f"Writing JSON to {output_path}")
+    with open(output_path, "w", encoding="utf8") as f:
+        indent = None if minify else "\t"
+        json.dump(gvas_file.dump(), f, indent=indent, cls=CustomEncoder)
+
+
+
+def convert_json_to_sav(filename: str, output_path: str):
+    print(f"Converting {filename} to SAV, saving to {output_path}")
+    if os.path.exists(output_path):
+        print(f"{output_path} already exists, this will overwrite the file")
+        if not confirm_prompt("Are you sure you want to continue?"):
+            exit(1)
+    print(f"Loading JSON from {filename}")
+    with open(filename, "r", encoding="utf8") as f:
+        data = json.load(f)
+    gvas_file = GvasFile.load(data)
+    print(f"Compressing SAV file")
+    if (
+        "Pal.PalWorldSaveGame" in gvas_file.header.save_game_class_name
+        or "Pal.PalLocalWorldSaveGame" in gvas_file.header.save_game_class_name
+    ):
+        save_type = 0x32
+    else:
+        save_type = 0x31
+    sav_file = compress_gvas_to_sav(
+        gvas_file.write(PALWORLD_CUSTOM_PROPERTIES), save_type
+    )
+    print(f"Writing SAV file to {output_path}")
+    with open(output_path, "wb") as f:
+        f.write(sav_file)
+
+
+def confirm_prompt(question: str) -> bool:
+    reply = None
+    while reply not in ("y", "n"):
+        reply = input(f"{question} (y/n): ").casefold()
+    return reply == "y"
+
+
+if __name__ == "__main__":
+    a = sys.argv[1]
+    b = sys.argv[2]
+    main(sav_file=a, guid=b)
